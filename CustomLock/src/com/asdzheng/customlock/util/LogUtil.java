1e8038a0448ff22de6ec558c6f710099641f10d4
richard_zheng <zhengjiabo@gmail.com>
2015-05-04 10:20:00 +0800

    Update LogUtil.java

--- CustomLock/src/com/asdzheng/customlock/util/LogUtil.java
+++ CustomLock/src/com/asdzheng/customlock/util/LogUtil.java
@@ -18,7 +18,7 @@ public class LogUtil {
 
     public static final int NOTHING = 6;
 
-    public static final int LEVEL = VERBOSE;
+    public static final int LEVEL = NOTHING;
 
     public static void v(String tag, String msg) {
         if (LEVEL <= VERBOSE) {
